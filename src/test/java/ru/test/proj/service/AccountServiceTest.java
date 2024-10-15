fffbe8a0cc866115df5ca1808d0891335f50da4a
kirill <kirill18092000@mail.ru>
2024-10-15 18:23:12 +0300

    Готово к сдаче

--- src/test/java/ru/test/proj/service/AccountServiceTest.java
+++ src/test/java/ru/test/proj/service/AccountServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.test.proj.exception.AccountTransferException;
@@ -17,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {
+        "spring.liquibase.enabled=false"
+})
 class AccountServiceTest {
 
     @Autowired
